b1dab272fe8688eab60586f74681f64706894654
Sylvain <sylvain.ramshaw@gmail.com>
2024-09-22 19:16:32 -0500

    fix

--- scripts/attach_pci_to_vm.sh
+++ scripts/attach_pci_to_vm.sh
@@ -11,15 +11,15 @@ VM_ID=`virsh list --title | tail -n +3 | grep $VM_NAME | awk '{print $1}'`
 ADDRESSES=($(lspci |grep $VENDOR:$PRODUCT | awk '{print $1}'))
 modprobe -r vfio_pci
 for ADDRESS in "${ADDRESSES[@]}"; do  echo "0000:$ADDRESS" > /sys/bus/pci/devices/0000:$ADDRESS/driver/unbind ; done
-echo $VENDOR $PRODUCT > /sys/bus/pci/drivers/vfio-pci/new_id
 modprobe vfio_pci ids=$VENDOR:$PRODUCT $EXTRA_MODEPROB_PARAMS
+echo $VENDOR $PRODUCT > /sys/bus/pci/drivers/vfio-pci/new_id
 for ADDRESS in "${ADDRESSES[@]}"
 do
   DOMAIN=0x0000
   FUNC=`echo $ADDRESS | awk -F[.:] '{print $3}' | echo 0x$(</dev/stdin)`
   SLOT=`echo $ADDRESS | awk -F[.:] '{print $2}' | echo 0x$(</dev/stdin)`
   BUS=` echo $ADDRESS | awk -F[.:] '{print $1}' | echo 0x$(</dev/stdin)`
-  sed -e "s/\[DOMAIN\]/$DOMAIN/"  -e "s/\[BUS\]/$BUS/" -e "s/\[SLOT\]/$SLOT/" -e "s/\[FUNC\]/$FUNC/" /usr/local/libvirt/dev_template.xml > /usr/local/libvirt/$ADDRESS.xml
+  sed -e "s/\[DOMAIN\]/$DOMAIN/"  -e "s/\[BUS\]/$BUS/" -e "s/\[SLOT\]/$SLOT/" -e "s/\[FUNC\]/$FUNC/" /usr/local/libvirt/vm_template.xml > /usr/local/libvirt/$ADDRESS.xml
   virsh attach-device $VM_ID /usr/local/libvirt/$ADDRESS.xml --current
   cat /usr/local/libvirt/$ADDRESS.xml
 done
